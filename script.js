b3dc7350b9cda81b7c742f3535eaf138df038371
drinkingstraw33 <60614681+drinkingstraw33@users.noreply.github.com>
2020-02-24 00:14:59 +0900

    archiving

--- script.js
+++ script.js
@@ -1,60 +1,29 @@
-var Outer = {
-    setTitleColor: function (color) {
-        $('#title').css('backgroundColor', color);
-    },
-    setHeaderColor: function (color) {
-        $('header').css('backgroundColor', color);
-    }
-}
-
 var Body = {
     setColor: function (color) {
-        $('body').css('color', color);
+        $('body').css('color', color)
     },
     setBackgroundColor: function (color) {
-        $('body').css('backgroundColor', color);
+        $('body').css('backgroundColor', color)
     }
 }
-
 var Links = {
     setColor: function (color) {
-        $('a').css('color', color);
-    }
-}
-
-var Footer = {
-    setInstarColor: function (color) {
-        $('.instar').css('backgroundColor', color);
-    },
-    setFaceColor: function (color) {
-        $('.face').css('backgroundColor', color);
-    },
-    setColor: function (color) {
-    $('footer').css('color', color);
+       $('a').css('color', color)
 }
 }
-
 function nightDayHandler(self) {
     var target = document.querySelector('body');
     if (self.value === '야간 모드로 전환') {
         self.value = '야간 모드 해제';
-        Outer.setTitleColor('#4E2700');
-        Outer.setHeaderColor('#272100');
-        Body.setBackgroundColor('#181800');
+        Body.setBackgroundColor('black');
         Body.setColor('white');
-        Links.setColor('#74BAEC');
-        Footer.setInstarColor('#008453');
-        Footer.setFaceColor('#522719');
-        Footer.setColor('#626671');
+        Links.setColor('powderblue');
+        alert('야간 모드!');
     } else {
         self.value = '야간 모드로 전환';
-        Outer.setTitleColor('gold');
-        Outer.setHeaderColor('palegoldenrod');
-        Body.setBackgroundColor('lightgoldenrodyellow');
+        Body.setBackgroundColor('white');
         Body.setColor('black');
-        Links.setColor('saddlebrown');
-        Footer.setInstarColor('pink');
-        Footer.setFaceColor('lightblue');
-        Footer.setColor('gray');
+        Links.setColor('blue');
+        alert('야간 모드 해제!');
     }
 }

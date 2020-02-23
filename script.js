2bf99881679abc4003f7e1f13761331c97daa8b5
drinkingstraw33 <60614681+drinkingstraw33@users.noreply.github.com>
2020-02-23 23:54:40 +0900

    design revmap... ana jukneunjul aratne!

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
+var Outer = {
+    setTitleColor: function (color) {
+        $('#title').css('backgroundColor', color);
+    },
+    setHeaderColor: function (color) {
+        $('header').css('backgroundColor', color);
+    }
+}
+
 var Body = {
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
+
 var Links = {
     setColor: function (color) {
-        var alist = document.querySelectorAll('a, .js');
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i += 1;
-        }
+        $('a').css('color', color);
     }
 }
+
+var Footer = {
+    setInstarColor: function (color) {
+        $('.instar').css('backgroundColor', color);
+    },
+    setFaceColor: function (color) {
+        $('.face').css('backgroundColor', color);
+    },
+    setColor: function (color) {
+    $('footer').css('color', color);
+}
+}
+
 function nightDayHandler(self) {
     var target = document.querySelector('body');
     if (self.value === '야간 모드로 전환') {
         self.value = '야간 모드 해제';
-        Body.setBackgroundColor('black');
+        Outer.setTitleColor('#4E2700');
+        Outer.setHeaderColor('#272100');
+        Body.setBackgroundColor('#181800');
         Body.setColor('white');
-        Links.setColor('powderblue');
-        alert('야간 모드!');
+        Links.setColor('#74BAEC');
+        Footer.setInstarColor('#008453');
+        Footer.setFaceColor('#522719');
+        Footer.setColor('#626671');
     } else {
         self.value = '야간 모드로 전환';
-        Body.setBackgroundColor('white');
+        Outer.setTitleColor('gold');
+        Outer.setHeaderColor('palegoldenrod');
+        Body.setBackgroundColor('lightgoldenrodyellow');
         Body.setColor('black');
-        Links.setColor('blue');
-        alert('야간 모드 해제!');
+        Links.setColor('saddlebrown');
+        Footer.setInstarColor('pink');
+        Footer.setFaceColor('lightblue');
+        Footer.setColor('gray');
     }
 }
